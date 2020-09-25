794d2c409acdac35624d07135509aba90d68f855
root <root@Mwakalingaz.localdomain>
2020-09-25 18:48:05 +0300

    - form arry added..

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -4,13 +4,15 @@ import { CommonModule } from '@angular/common';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { HomeComponent } from './home/home.component';
 import { ProductsComponent } from './products/products.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [ ProductsComponent, HomeComponent],
   imports: [
     CommonModule,
-    DashboardRoutingModule
+    DashboardRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class DashboardModule { }
--- src/app/modules/dashboard/home/home.component.ts
+++ src/app/modules/dashboard/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  public foodsForm: FormGroup
+
+  constructor(public fb:FormBuilder) { }
 
   ngOnInit(): void {
+    this.foodsForm = this.fb.group({
+      foods: this.fb.array([])
+    })
+    this.loadFoods()
+  }
+
+  get foods(){
+    return this.foodsForm.get('foods') as FormArray;
+  }
+
+  addFood(food=''){
+    this.foods.push(this.fb.control(food))
+  }
+
+  removeFood(i){
+    this.foods.removeAt(i)
+  }
+
+  onAddFoodClickHandler(){
+    this.addFood()
+  }
+
+  onRemoveFoodClickHandler(i){
+    console.log(i);
+
+    this.removeFood(i)
+  }
+
+  loadFoods(){
+
+    let foodsFromServer = [
+      {id: 1, value:"wali"},
+      {id: 2, value:"bamia"},
+      {id: 3, value:"andazi"},
+      {id: 4, value:"makande"},
+      {id: 5, value:"njegere"},
+    ]
+
+    foodsFromServer.forEach((food,index)=>{
+      this.addFood(food.value)
+    })
   }
 
 }
